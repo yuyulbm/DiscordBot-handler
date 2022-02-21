4ea412003202b6fda6651e1c91671bf1c6d4971e
Shinpi <82416698+Shinpi-Tekita@users.noreply.github.com>
2022-02-21 12:26:22 -0600

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,18 @@
+const { MessageEmbed, MessageAttachment } = require("discord.js");
+const emojis = require("../emojis")
+const client = require("../index");
+
+client.on("ready", async () => {
+let servers = client.guilds.cache.size
+let servercount = client.guilds.cache.reduce((a,b) => a+b.memberCount, 0)
+console.log(`Ready! Logged in as ${client.user.tag}\ncurrently in ${servers} servers and watching over ${servercount} members`);  
+
+let ready = new MessageEmbed()
+.setDescription(`Gentleman Is Back Online ${emojis.gentleman}`)
+.setColor("#6F8FAF")
+.setTimestamp()
+client.channels.cache.get('Your logs channel id').send({embeds: [ready]})
+console.log('Sent')
+
+client.user.setActivity(`/help | ${servers} servers`, { type: 'PLAYING' });
+});
