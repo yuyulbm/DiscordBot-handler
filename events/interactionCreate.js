fa734818d3cc1718cd4419d711413b04497a6926
Shinpi <82416698+Shinpi-Tekita@users.noreply.github.com>
2022-08-31 09:46:09 -0500

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,31 +1,57 @@
 const client = require("../index");
 
 client.on("interactionCreate", async (interaction) => {
-    // Slash Command Handling
-    if (interaction.isCommand()) {
+	// Slash Command Handling
+	if (interaction.isCommand()) {
 
-        const cmd = client.slashCommands.get(interaction.commandName);
-        if (!cmd)
-            return interaction.followUp({ content: "An error has occured " });
+		const cmd = client.slashCommands.get(interaction.commandName);
+		if (!cmd)
+			return interaction.followUp({
+				content: "An error has occured "
+			});
 
-        const args = [];
+		const args = [];
 
-        for (let option of interaction.options.data) {
-            if (option.type === "SUB_COMMAND") {
-                if (option.name) args.push(option.name);
-                option.options?.forEach((x) => {
-                    if (x.value) args.push(x.value);
-                });
-            } else if (option.value) args.push(option.value);
-        }
-        interaction.member = interaction.guild.members.cache.get(interaction.user.id);
+		for (let option of interaction.options.data) {
+			if (option.type === "SUB_COMMAND") {
+				if (option.name) args.push(option.name);
+				option.options?.forEach((x) => {
+					if (x.value) args.push(x.value);
+				});
+			} else if (option.value) args.push(option.value);
+		}
+		interaction.member = interaction.guild.members.cache.get(interaction.user.id);
 
-        cmd.run(client, interaction, args);
-    }
+		if (!interaction.memberPermissions.has(cmd.userPerms || []))
+			return interaction.reply({
+				embeds: [
+					new MessageEmbed()
+					.setTitle("Missing Permisssion")
+					.setDescription("My Apologies But You Do Not Have The Required Permissions To Use This Command.")
+					.addField("Required Permissions", `\`\`\`${cmd.userPerms.map((perm) => nicerPermissions(perm)).join("\n")}\`\`\``)
+					.setColor("#6F8FAF")
+				],
+				ephemeral: true
+			});
 
-    // Context Menu Handling
-    if (interaction.isContextMenu()) {
-        const command = client.slashCommands.get(interaction.commandName);
-        if (command) command.run(client, interaction);
-    }
+		if (!interaction.guild.me.permissions.has(cmd.botPerms || []))
+			return interaction.reply({
+				embeds: [
+					new MessageEmbed()
+					.setTitle("Missing Permisssion")
+					.setDescription("My Apologies But I Do Not Have The Required Permissions To Run This Command.")
+					.addField("Required Permissions", `\`\`\`${cmd.botPerms.map((perm) => nicerPermissions(perm)).join("\n")}\`\`\``)
+					.setColor("#6F8FAF")
+				],
+				ephemeral: true
+			});
+
+		cmd.run(client, interaction, args);
+	}
+
+	// Context Menu Handling
+	if (interaction.isContextMenu()) {
+		const command = client.slashCommands.get(interaction.commandName);
+		if (command) command.run(client, interaction);
+	}
 });
