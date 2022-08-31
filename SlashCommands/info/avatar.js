beaeabd20b814d987e03e1eb59b976deb4085c4c
Shinpi <82416698+Shinpi-Tekita@users.noreply.github.com>
2022-08-31 09:57:41 -0500

    Update avatar.js

--- SlashCommands/info/avatar.js
+++ SlashCommands/info/avatar.js
@@ -1,44 +1,52 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js')
- 
+const {
+	MessageEmbed,
+	MessageActionRow,
+	MessageButton
+} = require('discord.js')
+
 module.exports = {
-name: "avatar",
-description: "Get somones avatar",
-options: [
-{
-name: "user",
-description: "the person you want to view its avatar",
-type: "USER",
-required: false,
-},
-],
-run: async (client, interaction, args) => {
-const user = await interaction.options.getUser("user") || interaction.user;   
-const embed = new MessageEmbed()
-.setTitle(`${user.tag}'s  Avatar`)
-.setImage(user.displayAvatarURL({ dynamic: true, size: 1024 }))
-.setColor("#6F8FAF")
-.setTimestamp();
- 
-const linksRow = new MessageActionRow().addComponents(
- 
-  new MessageButton()
-  .setLabel("JPG")
- .setStyle("LINK")
-   .setURL(`${user.displayAvatarURL({format: "jpg",size: 1024,})}`),
-   new MessageButton()
-  .setLabel("PNG")
- .setStyle("LINK")
-   .setURL(`${user.displayAvatarURL({format: "png",size: 1024,})}`),
-    new MessageButton()
-  .setLabel("GIF")
- .setStyle("LINK")
-   .setURL(`${user.displayAvatarURL({format: "gif",size: 1024, dynamic: true,})}`),
-   new MessageButton()
-  .setLabel("WEBP")
- .setStyle("LINK")
-   .setURL(`${user.displayAvatarURL({format: "webp",size: 1024,})}`),
- )
- 
- interaction.reply({embeds: [embed], components: [linksRow]});
+	name: "avatar",
+	description: "Get somones avatar",
+	options: [{
+		name: "user",
+		description: "the person you want to view its avatar",
+		type: "USER",
+		required: false,
+	}, ],
+	run: async (client, interaction, args) => {
+		const user = await interaction.options.getUser("user") || interaction.user;
+		const embed = new MessageEmbed()
+			.setTitle(`${user.tag}'s  Avatar`)
+			.setImage(user.displayAvatarURL({
+				dynamic: true,
+				size: 1024
+			}))
+			.setColor("#6F8FAF")
+			.setTimestamp();
+
+		const linksRow = new MessageActionRow().addComponents(
+
+			new MessageButton()
+			.setLabel("JPG")
+			.setStyle("LINK")
+			.setURL(`${user.displayAvatarURL({format: "jpg",size: 1024,})}`),
+			new MessageButton()
+			.setLabel("PNG")
+			.setStyle("LINK")
+			.setURL(`${user.displayAvatarURL({format: "png",size: 1024,})}`),
+			new MessageButton()
+			.setLabel("GIF")
+			.setStyle("LINK")
+			.setURL(`${user.displayAvatarURL({format: "gif",size: 1024, dynamic: true,})}`),
+			new MessageButton()
+			.setLabel("WEBP")
+			.setStyle("LINK")
+			.setURL(`${user.displayAvatarURL({format: "webp",size: 1024,})}`),
+		)
+
+		interaction.reply({
+			embeds: [embed],
+			components: [linksRow]
+		});
+	}
 }
-} 
