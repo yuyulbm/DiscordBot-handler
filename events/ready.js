d8e776ee672d3ab2efa472d33d71ad2d17fb8b00
Shinpi <82416698+Shinpi-Tekita@users.noreply.github.com>
2022-08-31 09:53:08 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,18 +1,24 @@
-const { MessageEmbed, MessageAttachment } = require("discord.js");
-const emojis = require("../emojis")
+const {
+	MessageEmbed,
+	MessageAttachment
+} = require("discord.js");
 const client = require("../index");
 
 client.on("ready", async () => {
-let servers = client.guilds.cache.size
-let servercount = client.guilds.cache.reduce((a,b) => a+b.memberCount, 0)
-console.log(`Ready! Logged in as ${client.user.tag}\ncurrently in ${servers} servers and watching over ${servercount} members`);  
+	let servers = client.guilds.cache.size
+	let servercount = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
+	console.log(`Ready! Logged in as ${client.user.tag}\ncurrently in ${servers} servers and watching over ${servercount} members`);
 
-let ready = new MessageEmbed()
-.setDescription(`Bot Is Back Online ${emojis.emoji}`)
-.setColor("#6F8FAF")
-.setTimestamp()
-client.channels.cache.get('Your logs channel id').send({embeds: [ready]})
-console.log('Sent')
+	let ready = new MessageEmbed()
+		.setDescription(`Bot Is Back Online ${client.emojis.happy}`)
+		.setColor("#6F8FAF")
+		.setTimestamp()
+	client.channels.cache.get('Your logs channel id').send({
+		embeds: [ready]
+	})
+	console.log('Sent')
 
-client.user.setActivity(`/help | ${servers} servers`, { type: 'PLAYING' });
+	client.user.setActivity(`/help | ${servers} servers`, {
+		type: 'PLAYING'
+	});
 });
